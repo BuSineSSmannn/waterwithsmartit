e74deeb1463beab52fb3a1e3e1c09b1454cef654
Jason Dev <=>
2024-11-27 19:47:43 +0500

    gp

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Middleware\ApiAuthMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', static function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
